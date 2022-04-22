7c6e38ecdb94fbd235d5a45934d60a075f1a2370
Cemal Küller <cemalkuller@gmail.com>
2022-04-22 09:41:57 +0300

    make it better

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', '178.18.201.98'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'haydicard'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      database: env('DATABASE_NAME', 'incirio_haydikart'),
+      user: env('DATABASE_USERNAME', 'incirio_haydikart'),
+      password: env('DATABASE_PASSWORD', '?!}B1W_,{iXn'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
